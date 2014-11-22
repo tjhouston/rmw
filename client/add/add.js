eb812b0c2be5459a94da885646fccd0a0b3a8682
Tj Houston <tj@tjhouston.com>
2014-11-21 19:02:53 -0600

    Added some CSS and about page

--- client/add/add.js
+++ client/add/add.js
@@ -4,16 +4,19 @@ Template.add.events({
   "submit form": function (event, template) {
      var inputValue = event.target.new_product_name.value;
 	 var clientID = "NOTDEFINED";
+	 var upc = event.target.upc.value;
 	 var purchaseDate = event.target.purchaseDate.value;
 	 var expirationDate = event.target.expirationDate.value;
 	 var descriptionInput = event.target.descriptionInput.value;
 	 var itemCost = event.target.itemCost.value;
+	 var receipt = event.target.receipt.value;
+	 var purchaseLocation = event.target.purchaseLocation.value;
 	 
 	 
 	 
    //  alert(inputValue);
 	 
 	 clientID=Meteor.userId();
-	 Products.insert({clientID:clientID , productName:inputValue, purchaseDate:purchaseDate, expirationDate:expirationDate, descriptionInput:descriptionInput,itemCost:itemCost });
+	 Products.insert({clientID:clientID , productName:inputValue, purchaseDate:purchaseDate, expirationDate:expirationDate, descriptionInput:descriptionInput,itemCost:itemCost,receipt:receipt, purchaseLocation:purchaseLocation, upc:upc });
 	   }
 });
