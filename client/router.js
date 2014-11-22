f141904e561e40646625f43ca3f97ecee28da45a
Tj Houston <tj@tjhouston.com>
2014-11-22 12:27:28 -0600

    fixed sync issues

--- client/router.js
+++ client/router.js
@@ -6,21 +6,14 @@ layoutTemplate: 'layout'
 
 Router.map (function(){
 	this.route('home',{path:'/'});
-<<<<<<< Updated upstream
 	this.route('about',{path:'/about'});
 	this.route('products',{path:'/products'});
 	this.route('add',{path:'/add'});
 	this.route('reminders',{path:'/reminders'});
-	this.route('modal',{path:'/modal'});
-=======
-    this.route('about',{path:'/about'});
-    this.route('products',{path:'/products'});
-    this.route('add',{path:'/add'});
 	this.route('email',{path:'/email'});
 	
 	
 	
 	
 
->>>>>>> Stashed changes
 })
