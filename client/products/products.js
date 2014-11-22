6ecf06e3a4d5409eee24883f13e90eb43c005785
Scott James Krieder <skrieder@iit.edu>
2014-11-21 18:26:31 -0600

    User can only see their items.

--- client/products/products.js
+++ client/products/products.js
@@ -11,8 +11,10 @@ Template.products.helpers({
   products: function () {
     // this helper returns a cursor of
     // all of the posts in the collection
-  	console.log("in products.helpers products.js");
-    return Products.find();
-	
+    console.log("in products.helpers products.js");
+    
+    myID=Meteor.userId();
+
+    return Products.find({ clientID: myID});	
   }
 });
