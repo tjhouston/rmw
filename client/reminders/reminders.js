ef666a69f46bb309b471868a4b0ae21513893451
Scott James Krieder <skrieder@iit.edu>
2014-11-22 12:25:05 -0600

    reminders work for admin page

--- client/reminders/reminders.js
+++ client/reminders/reminders.js
@@ -1,13 +1,20 @@
 console.log("In reminders.js");
 
 Template.reminders.helpers({
-  reminders: function () {
-    // this helper returns a cursor of
-    // all of the posts in the collection
-    console.log("in reminders.helpers reminders.js");
-    
-    myID=Meteor.userId();
+	reminders: function () {
+	    // this helper returns a cursor of
+	    // all of the posts in the collection
+	    console.log("in reminders.helpers reminders.js");
+	    
+	    myID=Meteor.userId();
 
-    return Products.find(); // find all the products	
-  }
-});
+            var d = new Date();
+            var y = d.getFullYear();
+            var m = d.getMonth()+1;
+            var n = d.getDate()+7;
+            console.log("A week from today's date is: " + y + "-" + m + "-" + n);
+            var weekFromToday = y + "-" + m + "-" + n;
+	    
+	    return Products.find({expirationDate: weekFromToday}); // find all the products
+	}
+    });
